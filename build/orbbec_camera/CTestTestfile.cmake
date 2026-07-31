5a24682b01bf6167d9062595abeb9f9ef7f20a2f
Mia <2982023833@qq.com>
2026-07-31 12:07:51 +0800

    untrack build install log directories

--- build/orbbec_camera/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mia/data/Project_Backup/cylinder_project/src/OrbbecSDK_ROS2-2-main/orbbec_camera
-# Build directory: /home/mia/data/Project_Backup/cylinder_project/build/orbbec_camera
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/orbbec_camera_msgs/orbbec_camera_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mia/data/Project_Backup/cylinder_project/build/orbbec_camera_msgs/orbbec_camera_msgs__py
-# Build directory: /home/mia/data/Project_Backup/cylinder_project/build/orbbec_camera_msgs/orbbec_camera_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
